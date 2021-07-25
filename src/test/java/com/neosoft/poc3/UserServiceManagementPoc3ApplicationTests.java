6f3951fa603b952fb08fb154658d05fafea22be0
vitt2411 <84367640+vitt2411@users.noreply.github.com>
2021-07-25 13:16:22 +0530

    56

--- src/test/java/com/neosoft/poc3/UserServiceManagementPoc3ApplicationTests.java
+++ src/test/java/com/neosoft/poc3/UserServiceManagementPoc3ApplicationTests.java
@@ -1,13 +1,12 @@
-package com.neosoft.poc3;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UserServiceManagementPoc3ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.neosoft.poc3;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class UserServiceManagementPoc3ApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
